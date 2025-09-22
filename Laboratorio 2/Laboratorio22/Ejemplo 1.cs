0c338d7ecb42978512552a6c408546f4db816076
itssmarii <itss.marian3016@gmail.com>
2025-09-22 09:23:25 -0500

    Add files via upload

---
+++ Laboratorio 2/Laboratorio22/Ejemplo 1.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            // Variables de instancia de clase
+            client.FirstName = "María";
+            client.LastName = "González";
+            client.age = 20;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+    }
+
+    public class Client
+    {
+        // Declarando variables de instancia en clase
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string age { get; set; }
+
+        public string GetFullName());
+        {
+            // Utilizando variables de instancia dentro de metodo clase
+            return FirstName + " " + LastName;
+        }
+            
+        
+    }
+}
